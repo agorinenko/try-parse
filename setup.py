7eb0021567907f73c1cc1a20a1f79918698c7c2d
a.gorinenko <a.gorinenko@itransition.com>
2019-11-27 18:28:03 +0300

    0.0.5 version

--- setup.py
+++ setup.py
@@ -19,5 +19,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
     python_requires='>=3.7',
 )
